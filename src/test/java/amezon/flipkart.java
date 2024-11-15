b3530cd54038d7b38119a5d734f880ab3306c305
abhisheksaklani28 <asaklani008@gmail.com>
2024-11-15 10:00:19 +0530

    code complete

--- src/test/java/amezon/flipkart.java
+++ src/test/java/amezon/flipkart.java
@@ -4,4 +4,9 @@ public class flipkart {
 	public void addToCart() {
 		System.out.println("addto cart on my flipkart");
 	}
+
+	public void addToCarts() {
+		System.out.println("addto carts on my flipkart");
+	}
+
 }
